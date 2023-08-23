2483bee7303a065ef5b32552a76e2e0d5b6401f8
unknown <prasadsp2001@gmail.com>
2023-08-23 16:15:50 +0530

    Added add func

--- sample.java
+++ sample.java
@@ -1,5 +1,11 @@
 public class sample{
+
+    private int add(int a,int b){
+        return a+b;
+    }
     public static void main(String[] args){
         System.out.println("Hello");
+        sample obj = new sample();
+        System.out.println(obj.add(2,3));
     }
 }
